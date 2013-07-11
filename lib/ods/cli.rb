b25f13e1af73c9119c95972ecec2d22ea9863288
Mike Park <mikep@quake.net>
2013-07-11 13:26:39 +0200

    use official class methods, and not the alias versions
    alias versions aren't available in rails 3.2.3 with ruby 1.9.2 for some reason

--- lib/ods/cli.rb
+++ lib/ods/cli.rb
@@ -3,9 +3,9 @@ require 'csv'
 
 module Ods
   class Cli < Thor
-    option :sheet, default: 0
-    option :file, required: true
-    option :verbose
+    method_option :sheet, default: 0
+    method_option :file, required: true
+    method_option :verbose
     desc 'csv', 'dumps --sheet x (0 by default) in csv format'
     def csv
       file = Ods::File.open(options[:file])
